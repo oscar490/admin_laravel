c27ec20b9b16e8eea8bf3769ac7d40d9ca99c624
Óscar Vega Herrera <oscarvegaherrera59@gmail.com>
2020-05-12 23:53:59 +0200

    Carga inicial

---
+++ resources/views/admin/users/listado/index.blade.php
@@ -0,0 +1,15 @@
+
+
+@extends('admin.general.layout')
+
+@section('title-module', 'Usuarios')
+
+@section('content')
+
+    <div class="row">
+        <div class="col-md-12">
+            <p>Usuarios</p>
+        </div>
+    </div>
+
+@endsection
